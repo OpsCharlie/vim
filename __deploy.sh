cfbba882b2d89a8d79aa79ae2e5c23064fd9c5e4
Carl <verstraete.carl@gmail.com>
2016-03-30 16:56:57 +0200

    added deploy script

---
+++ __deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+P=$1
+DIR=$(dirname $(readlink -f $0))
+
+if [ -z $P ]; then
+    echo copy files to homedir
+    mv ~/.vim ~/.vim.ori
+    mv ~/.vimrc ~/.vimrc.ori
+    ln -s $DIR/vim ~/.vim
+    ln -s $DIR/vimrc ~/.vimrc
+    exit $?
+fi
+
+if [ "$(expr match "$P" '.*\(:\)')" = ":" ]; then
+    echo "Usage:"
+    echo "$0               to deploy local"
+    echo "$0 user@host     to deploy remote"
+    exit 1
+fi
+
+rsync -rptvz --delete --exclude ".git" $DIR/vim/ $P:~/.vim
+rsync -rptvz --delete --exclude ".git" $DIR/vimrc $P:~/.vimrc
+
+
