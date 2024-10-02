1f76cb44aea136321c85033ce75a88258ea3eac2
CAVE <cave@sofico.be>
2024-10-02 11:56:18 +0200

    updat hardtime

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,4 +1,15 @@
 return {
-   "m4xshen/hardtime.nvim",
-   opts = {}
+	"m4xshen/hardtime.nvim",
+    -- Establish good command workflow and quit bad habit.
+	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+	opts = {
+		disable_mouse = false,
+		max_count = 5,
+		disabled_keys = {
+			["<Up>"] = { "" },
+			["<Down>"] = { "" },
+			["<Left>"] = { "" },
+			["<Right>"] = { "" },
+		},
+	},
 }
