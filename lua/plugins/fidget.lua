0b5f9f77f8334123625e29af450f8eb53c60c931
CAVE <cave@sofico.be>
2024-10-02 12:03:26 +0200

    Add fidget

---
+++ lua/plugins/fidget.lua
@@ -0,0 +1,9 @@
+return {
+    "j-hui/fidget.nvim",
+    -- Extensible UI for Neovim notifications and LSP progress messages.
+    lazy = true,
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+        -- options
+    },
+}
