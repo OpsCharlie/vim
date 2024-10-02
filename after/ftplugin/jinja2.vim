2729189db7ff99ea987f3db8ea19ecadddb24f72
CAVE <cave@sofico.be>
2024-10-02 11:54:06 +0200

    Add matchup config for jinja2

---
+++ after/ftplugin/jinja2.vim
@@ -0,0 +1,3 @@
+" Set the match_words buffer variable for Jinja2 files
+let b:match_words = '\<if\>:\<else\>:\<endif\>,'
+                \ . '\<for\>:\<endfor\>'
--- lua/plugins/vim-matchup.lua
+++ lua/plugins/vim-matchup.lua
@@ -1,4 +1,11 @@
 return {
-    "andymass/vim-matchup",
-    event = "BufReadPost"
+	"andymass/vim-matchup",
+	-- match-up is a plugin that lets you highlight, navigate, and operate on sets of matching text.
+	event = "BufReadPost",
+	config = function()
+		vim.g.matchup_matchparen_offscreen = { method = "popup" }
+
+		-- Enable matchup override for custom syntax highlighting
+		vim.g.matchup_override_vim_regex_highlight = 1
+	end,
 }
