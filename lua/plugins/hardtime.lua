c81ae1de1eeca859b0e85a4afd3e491724058680
CAVE <cave@sofico.be>
2024-10-03 12:19:11 +0200

    Add nvim notify

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,15 +1,16 @@
 return {
-	"m4xshen/hardtime.nvim",
-    -- Establish good command workflow and quit bad habit.
-	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-	opts = {
-		disable_mouse = false,
-		max_count = 5,
-		disabled_keys = {
-			["<Up>"] = { "" },
-			["<Down>"] = { "" },
-			["<Left>"] = { "" },
-			["<Right>"] = { "" },
-		},
-	},
+  "m4xshen/hardtime.nvim",
+  -- Establish good command workflow and quit bad habit.
+  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  opts = {
+    disable_mouse = false,
+    max_count = 5,
+    max_time = 100,
+    disabled_keys = {
+      ["<Up>"] = { "" },
+      ["<Down>"] = { "" },
+      ["<Left>"] = { "" },
+      ["<Right>"] = { "" },
+    },
+  },
 }
