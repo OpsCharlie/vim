eaecf4550dcfa327e75e492c661493860d686628
OpsCharlie <verstraete.carl@gmail.com>
2025-02-02 10:44:39 +0100

    Update which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,24 +1,23 @@
 return {
-    "folke/which-key.nvim",
-    -- WhichKey helps you remember your Neovim keymaps
-    dependencies = {
-        "echasnovski/mini.nvim",
-    },
-    lazy = true,
-    keys = { "<leader>", "<c-r>", "<c-w>", '"', "'", "`", "c", "v", "g", "z" },
-    init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 300
-    end,
-    config = function()
-        require("which-key").setup({
-            preset = "modern",
-            -- window = {
-            --   border = "single",      -- none, single, double, shadow
-            --   position = "bottom",  -- bottom, top
-            --   margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-            --   padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-            -- },
-        })
-    end,
+  "folke/which-key.nvim",
+  -- WhichKey helps you remember your Neovim keymaps
+  lazy = false,
+  keys = {
+    { "<leader>?", function() require("which-key").show({ global = false }) end, desc = "Buffer Local Keymaps (which-key)", },
+  },
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  config = function()
+    require("which-key").setup({
+      preset = "modern",
+      -- window = {
+      --   border = "single",      -- none, single, double, shadow
+      --   position = "bottom",  -- bottom, top
+      --   margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+      --   padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+      -- },
+    })
+  end,
 }
