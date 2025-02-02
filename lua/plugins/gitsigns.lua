dae3032ab75a8f607043fab440d204002faeb31a
OpsCharlie <verstraete.carl@gmail.com>
2025-02-02 13:40:12 +0100

    Add leader keys for gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,6 +2,16 @@ return {
   "lewis6991/gitsigns.nvim",
   -- Git integration for buffers
   ft = { "gitcommit", "diff" },
+  keys = {
+    {"<leader>gj", mode = "n", "<cmd>Gitsigns next_hunk<CR>", desc = "Git Next Hunk" },
+    {"<leader>gk", mode = "n", "<cmd>Gitsigns prev_hunk<CR>" , desc = "Git Previous Hunk" },
+    {"<leader>gs", mode = "n", "<cmd>Gitsigns stage_hunk<CR>", desc = "Git Stage Hunk" },
+    {"<leader>gu", mode = "n", "<cmd>Gitsigns undo_stage_hunk<CR>", desc = "Git Undo Stage Hunk" },
+    {"<leader>gr", mode = "n", "<cmd>Gitsigns reset_hunk<CR>", desc = "Git Reset Hunk" },
+    {"<leader>gR", mode = "n", "<cmd>Gitsigns reset_buffer<CR>", desc = "Git Reset buffer" },
+    {"<leader>gp", mode = "n", "<cmd>Gitsigns preview_hunk<CR>", desc = "Git Preview hunk" },
+    {"<leader>gb", mode = "n", "<cmd>Gitsigns blame_line<CR>", desc = "Git Blame line" },
+  },
   init = function()
     -- load gitsigns only when a git file is opened
     vim.api.nvim_create_autocmd({ "BufRead" }, {
