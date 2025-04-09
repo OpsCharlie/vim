26a817db8241d55448bfcf6c2cf0523e11cbefcc
CAVE <cave@sofico.be>
2025-04-09 14:35:05 +0200

    Add copilot chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,12 +1,30 @@
 return {
+  {
     "github/copilot.vim",
     lazy = true,
     event = { "BufReadPost", "BufNewFile" },
-    config = function ()
-      vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+    config = function()
+      vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
         expr = true,
-        replace_keycodes = false
+        replace_keycodes = false,
       })
       vim.g.copilot_no_tab_map = true
     end,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" },
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+    build = "make tiktoken",
+    keys = {
+      { "<leader>cc", ":CopilotChatToggle<CR>", desc = "Toggle Copilot Chat" }
+    },
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 }
