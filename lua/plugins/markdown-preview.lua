436e3ef3aea57132017ddb920e02ebecfc0b16e1
OpsCharlie <verstraete.carl@gmail.com>
2025-05-04 10:45:56 +0200

    update markdown config

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -14,12 +14,8 @@ return {
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
   build = function(plugin)
-    if vim.fn.executable("npx") then
-      vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
-    else
-      vim.cmd([[Lazy load markdown-preview.nvim]])
-      vim.fn["mkdp#util#install"]()
-    end
+    local install_cmd = vim.fn.executable("npx") and "npx --yes yarn install" or "npm install"
+    vim.cmd("!cd " .. plugin.dir .. "/app && " .. install_cmd)
   end,
   init = function()
     if vim.fn.executable("npx") then
