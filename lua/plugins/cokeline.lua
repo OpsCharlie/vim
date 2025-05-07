11cc8599aed4d65bf9555508b7402f8d74fa5adf
CAVE <cave@sofico.be>
2025-05-07 15:27:26 +0200

    update some config

--- lua/plugins/cokeline.lua
+++ lua/plugins/cokeline.lua
@@ -1,32 +1,70 @@
 return {
-    "willothy/nvim-cokeline",
-    -- A Neovim bufferline
-    dependencies = {
-        "nvim-lua/plenary.nvim", -- Required for v0.4.0+
-        "nvim-tree/nvim-web-devicons", -- If you want devicons
-    },
-    config = function()
-        local get_hex = require("cokeline.hlgroups").get_hl_attr
-        require("cokeline").setup({
-            default_hl = {
-                fg = function(buffer)
-                    if buffer.is_modified and buffer.is_focused then
-                        return "#ff0000"
-                    elseif buffer.is_focused then
-                        return get_hex("Normal", "fg")
-                    elseif buffer.is_modified then
-                        return "#b30000"
-                    else
-                        return get_hex("Comment", "fg")
-                    end
-                end,
-            },
-        })
-    end,
-    vim.keymap.set("n", "<S-TAB>", function()
-        require("cokeline.mappings").by_step("focus", "-1")
-    end, { desc = "Previous buffer" }),
-    vim.keymap.set("n", "<TAB>", function()
-        require("cokeline.mappings").by_step("focus", "1")
-    end, { desc = "Next buffer" }),
+  "willothy/nvim-cokeline",
+  -- A Neovim bufferline
+  dependencies = {
+    "nvim-lua/plenary.nvim",     -- Required for v0.4.0+
+    "nvim-tree/nvim-web-devicons", -- If you want devicons
+  },
+  config = function()
+    local get_hex = require("cokeline.hlgroups").get_hl_attr
+    require("cokeline").setup({
+      default_hl = {
+        fg = function(buffer)
+          if buffer.is_modified and buffer.is_focused then
+            return "#ff0000"
+          elseif buffer.is_focused then
+            return get_hex("Normal", "fg")
+          elseif buffer.is_modified then
+            return "#b30000"
+          else
+            return get_hex("Comment", "fg")
+          end
+        end,
+      },
+      components = {
+        {
+          text = "｜",
+        },
+        {
+          text = function(buffer)
+            return buffer.devicon.icon
+          end,
+          fg = function(buffer)
+            return buffer.devicon.color
+          end,
+        },
+        {
+            text = function(buffer)
+                local diagnostics = vim.diagnostic.get(buffer.number)
+                local errors = #vim.tbl_filter(function(d)
+                    return d.severity == vim.diagnostic.severity.ERROR
+                end, diagnostics)
+                return errors > 0 and "  " .. errors .. " " or ""
+            end,
+            fg = "#ff0000",
+        },
+        {
+          text = function(buffer)
+            return buffer.filename .. " "
+          end,
+          bold = function(buffer)
+            return buffer.is_focused
+          end,
+        },
+        {
+          text = " ",
+        },
+        {
+            text = "",
+            delete_buffer_on_left_click = true,
+        },
+      },
+    })
+  end,
+  vim.keymap.set("n", "<S-TAB>", function()
+    require("cokeline.mappings").by_step("focus", "-1")
+  end, { desc = "Previous buffer" }),
+  vim.keymap.set("n", "<TAB>", function()
+    require("cokeline.mappings").by_step("focus", "1")
+  end, { desc = "Next buffer" }),
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,84 +1,34 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    -- A blazing fast and easy to configure Neovim statusline
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        require("lualine").setup({
-            options = {
-                theme = "auto",
-                -- theme = "catppuccin",
-                globalstatus = true,
-            },
-            sections = {
-                -- lualine_c = {
-                --   {
-                --     "filename",
-                --     path = 2,
-                --     -- color = "#B22222"
-                --   },
-                -- },
-                lualine_c = {
-                    {
-                        function()
-                            local bg = "#404040" -- not modified
-                            if vim.bo.modified then
-                                bg = "#B22222" -- unsaved
-                            elseif vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(0), "readonly") then
-                                bg = "#b26a22" -- readonly
-                            end
-                            vim.cmd("hi! lualine_filename_status guibg=" .. bg)
-                            return vim.fn.expand("%:p")
-                        end,
-                        "filename",
-                        color = "lualine_filename_status",
-                    },
-                },
-            },
-        })
-    end,
+  "nvim-lualine/lualine.nvim",
+  -- A blazing fast and easy to configure Neovim statusline
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "auto",
+        globalstatus = true,
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_c = {
+          {
+            function()
+              local bg = "#404040" -- not modified
+              if vim.bo.modified then
+                bg = "#B22222" -- unsaved
+              elseif vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(0), "readonly") then
+                bg = "#b26a22" -- readonly
+              end
+              vim.cmd("hi! lualine_filename_status guibg=" .. bg)
+              return vim.fn.expand("%:p")
+            end,
+            "filename",
+            color = "lualine_filename_status",
+          },
+        },
+      },
+    extensions = { "nvim-tree", "quickfix", "nvim-dap-ui", "toggleterm" },
+    })
+  end,
 }
-
--- return {
---   "nvim-lualine/lualine.nvim",
---   dependencies = { "nvim-tree/nvim-web-devicons" },
---   config = function()
---     local custom_fname = require("lualine.components.filename"):extend()
---     local highlight = require("lualine.highlight")
---     local default_status_colors = { saved = "#228B22", modified = "#C70039" }
---
---     function custom_fname:init(options)
---       custom_fname.super.init(self, options)
---       self.status_colors = {
---         saved = highlight.create_component_highlight_group(
---           { bg = default_status_colors.saved },
---           "filename_status_saved",
---           self.options
---         ),
---         modified = highlight.create_component_highlight_group(
---           { bg = default_status_colors.modified },
---           "filename_status_modified",
---           self.options
---         ),
---       }
---       if self.options.color == nil then
---         self.options.color = ""
---       end
---     end
---
---     function custom_fname:update_status()
---       local data = custom_fname.super.update_status(self)
---       data = highlight.component_format_highlight(
---         vim.bo.modified and self.status_colors.modified or self.status_colors.saved
---       ) .. data
---       return data
---     end
---
---     require("lualine").setup({
---       lualine_c = {custom_fname},
---       -- options = {
---       --   theme = "catppuccin",
---       --   globalstatus = true,
---       -- },
---     })
---   end,
--- }
