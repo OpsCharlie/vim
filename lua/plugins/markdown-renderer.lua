227d4df8916844c5dea4c76fbe69dd94cea149dd
CAVE <cave@sofico.be>
2025-05-12 10:31:25 +0200

    Update MD renderer

--- lua/plugins/markdown-renderer.lua
+++ lua/plugins/markdown-renderer.lua
@@ -1,10 +1,23 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  -- Plugin to improve viewing Markdown files in Neovim
-  lazy = true,
-  ft = { "markdown" },
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {},
+	"MeanderingProgrammer/render-markdown.nvim",
+	-- Plugin to improve viewing Markdown files in Neovim
+	lazy = true,
+	ft = { "markdown" },
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
+	opts = {
+		heading = {
+			enabled = true,
+			sign = false,
+			icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+			position = "inline",
+			width = "block",
+		},
+		bullet = {
+			enabled = true,
+      -- https://www.alt-codes.net/math-symbols-list
+			icons = { "•", "•" },
+		},
+	},
 }
