bf53db8595f890d0986f4a4e3d075226e426ccfd
OpsCharlie <verstraete.carl@gmail.com>
2025-06-08 15:57:20 +0200

    Update deploy script

--- __deploy.sh
+++ __deploy.sh
@@ -1,19 +1,28 @@
-#!/bin/bash
+BIN_DIR=~/.local/bin
+[[ -d $BIN_DIR ]] || mkdir -p  $BIN_DIR
 
-P=$1
 DIR=$(dirname "$(readlink -f "$0")")
 
-if [ -z "$P" ]; then
-    echo copy files to homedir
-    rsync -a --delete "$DIR" ~/.config/nvim/
-    exit $?
-fi
+curl -s https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage -L -o $BIN_DIR/nvim
 
-if [ "$(expr match "$P" '.*\(:\)')" = ":" ]; then
-    echo "Usage:"
-    echo "$0               to deploy local"
-    echo "$0 user@host     to deploy remote"
-    exit 1
-fi
+chmod +x $BIN_DIR/nvim
 
-ln -s "$DIR"/vim ~/.config/nvim
+cd "$DIR" || exit
+
+NVIM_CONFIG="$HOME/.config/nvim"
+if [ -d "$NVIM_CONFIG" ] && [ ! -L "$NVIM_CONFIG" ]; then
+    echo "Backing up existing nvim config to $NVIM_CONFIG.bak"
+    mv "$NVIM_CONFIG" "$NVIM_CONFIG.bak"
+    ln -s "$DIR" "$NVIM_CONFIG"
+elif [ -L "$NVIM_CONFIG" ]; then
+    TARGET=$(readlink -f "$NVIM_CONFIG")
+    if [ "$TARGET" = "$DIR" ]; then
+        echo "$NVIM_CONFIG is already correctly linked."
+    else
+        echo "$NVIM_CONFIG is a symlink to $TARGET, updating to $DIR"
+        rm "$NVIM_CONFIG"
+        ln -s "$DIR" "$NVIM_CONFIG"
+    fi
+else
+    ln -s "$DIR" "$NVIM_CONFIG"
+fi
