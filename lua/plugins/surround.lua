7a43644d51381bdbfadca69927a4c74f87afb37c
OpsCharlie <verstraete.carl@gmail.com>
2025-07-06 11:39:39 +0200

    Add mini.surround

---
+++ lua/plugins/surround.lua
@@ -0,0 +1,20 @@
+return {
+  "echanovski/mini.surround",
+  -- Fast and feature-rich surrounding.
+  version = '*',
+  lazy = true,
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {
+    mappings = {
+      add = "<leader>sa",
+      delete = "<leader>sd",
+      find = "<leader>sf",
+      find_left = "<leader>sF",
+      highlight = "<leader>sh",
+      replace = "<leader>sr",
+      update_n_lines = "<leader>sn",
+    },
+    search_method = "cover",
+    n_lines = 20,
+  },
+}
