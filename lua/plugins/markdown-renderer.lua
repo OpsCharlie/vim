0cc79b7afdf135e75f9f64141bc3fbd142b452c6
OpsCharlie <verstraete.carl@gmail.com>
2025-08-07 12:40:21 +0200

    Add ToC for markdown

--- lua/plugins/markdown-renderer.lua
+++ lua/plugins/markdown-renderer.lua
@@ -1,23 +1,37 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  -- Plugin to improve viewing Markdown files in Neovim
-  lazy = true,
-  ft = { "markdown" },
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {
-    heading = {
-      enabled = true,
-      sign = false,
-      icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
-      position = "inline",
-      width = "block",
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    -- Plugin to improve viewing Markdown files in Neovim
+    lazy = true,
+    ft = { "markdown" },
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      heading = {
+        enabled = true,
+        sign = false,
+        icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+        position = "inline",
+        width = "block",
+      },
+      bullet = {
+        enabled = true,
+        -- https://www.alt-codes.net/math-symbols-list
+        icons = { "•", "•" },
+      },
     },
-    bullet = {
-      enabled = true,
-      -- https://www.alt-codes.net/math-symbols-list
-      icons = { "•", "•" },
+  },
+  {
+    'mzlogin/vim-markdown-toc',
+    -- Generate table of contents for Markdown files
+    cmd = { 'GenTocGFM', 'GenTocRedcarpet', 'GenTocGitLab', 'UpdateToc' },
+    ft = 'markdown',
+    keys = {
+      { '<leader>mo', '<cmd>UpdateToc<CR>', desc = 'Update table of contents' },
     },
+    init = function()
+      vim.g.vmt_auto_update_on_save = 0
+    end,
   },
 }
