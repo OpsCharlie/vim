7ead9f4fbddd0041652836aaacc3aabbf645d7cd
OpsCharlie <verstraete.carl@gmail.com>
2025-08-30 13:27:47 +0200

    Change key map

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -3,7 +3,7 @@ return {
   lazy = true,
   cmd = { "Outline", "OutlineOpen" },
   keys = {
-    { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+    { "<leader>co", "<cmd>Outline<CR>", desc = "Code Outline Toggle" },
   },
   opts = {},
 }
