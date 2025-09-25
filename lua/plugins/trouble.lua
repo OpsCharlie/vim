d34044ed74e1fde6fd3388870632994f7c97f4cc
OpsCharlie <verstraete.carl@gmail.com>
2025-09-25 08:39:25 +0200

    Migrate trouble

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,13 +1,14 @@
-return {
-  "folke/trouble.nvim",
-  -- A pretty diagnostics, references, telescope results, quickfix and location list to help you solve all the trouble your code is causing.
-  cmd = "Trouble",
-  opts = {},
-  keys = {
-    {
-      "<leader>dD",
-      "<cmd>Trouble diagnostics toggle<cr>",
-      desc = "Diagnostics (Trouble)",
-    },
-  },
-}
+return {}
+-- return {
+--   "folke/trouble.nvim",
+--   -- A pretty diagnostics, references, telescope results, quickfix and location list to help you solve all the trouble your code is causing.
+--   cmd = "Trouble",
+--   opts = {},
+--   keys = {
+--     {
+--       "<leader>dD",
+--       "<cmd>Trouble diagnostics toggle<cr>",
+--       desc = "Diagnostics (Trouble)",
+--     },
+--   },
+-- }
