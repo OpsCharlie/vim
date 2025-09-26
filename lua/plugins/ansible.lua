67c00e6e0f7d6cd32d626d342cf2ec475ee821f9
OpsCharlie <verstraete.carl@gmail.com>
2025-09-26 14:21:13 +0200

    Update ansible

--- lua/plugins/ansible.lua
+++ lua/plugins/ansible.lua
@@ -7,12 +7,13 @@ return {
     vim.g.ansible_unindent_after_newline = 1
     vim.g.ansible_extra_keywords_highlight = 1
     vim.opt.colorcolumn = "160"
+    local filename = vim.fn.expand("%:t:r")                  -- Get filename without path or extension
     vim.keymap.set({ "n", "i" }, "<leader>n", function()
-      local filename = vim.fn.expand("%:t:r")             -- Get filename without path or extension
       vim.api.nvim_put({ filename .. " | " }, "c", true, true) -- Insert at cursor
     end, { desc = "Add name prefix", silent = true })
-    vim.keymap.set("n", "<leader>N", ":%s/- name: /- name: %<BS><BS><BS><BS> | /<CR>",
-      { desc = "Add buffer name prefix", silent = true })
+    vim.keymap.set("n", "<leader>N", function()
+      vim.cmd("silent! %s/- name: /- name: " .. filename .. " | /g")
+    end, { desc = "Add buffer name prefix", silent = true })
     vim.keymap.set("n", "<leader>a", "wbiansible.builtin.<ESC>",
       { desc = "Add FQCN for builtin module", silent = true })
     vim.keymap.set('n', '<leader>ca', ':!ansible-lint --fix %<CR>', { desc = "Code Action ansiblelint", buffer = true })
@@ -20,9 +21,22 @@ return {
     -- Set 'K' mapping for every yaml.ansible buffer using autocmd
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "yaml.ansible",
-      callback = function(args)
-        vim.keymap.set('n', 'K', function()
-          local cword = vim.fn.expand("<cword>")
+      callback = function(ev)
+        vim.keymap.set({ 'n', 'v' }, 'K', function()
+          local mode = vim.fn.mode(true)
+          local module
+          if mode:sub(1, 1) == 'v' or mode:sub(1, 1) == 'V' then
+            -- Reselect visual selection
+            local save_reg = vim.fn.getreg('"')
+            vim.cmd('normal! "vy')
+            module = vim.fn.getreg('v')
+            vim.fn.setreg('"', save_reg)
+            module = module and module:gsub("\n", "") or ""
+          else
+            local cword = vim.fn.expand("<cword>")
+            module = cword:match("(.+)%..+") or cword
+          end
+          if not module or module == "" then return end
           local buf = vim.api.nvim_create_buf(false, true)
           local width = math.floor(vim.o.columns * 0.8)
           local height = math.floor(vim.o.lines * 0.8)
@@ -39,7 +53,7 @@ return {
           })
           local chan = vim.api.nvim_open_term(buf, {})
           vim.fn.jobstart(
-            { "sh", "-c", "ANSIBLE_FORCE_COLOR=1 TERM=xterm-256color PAGER=cat ansible-doc " .. vim.fn.shellescape(cword) },
+            { "sh", "-c", "ANSIBLE_FORCE_COLOR=1 TERM=xterm-256color PAGER=cat ansible-doc " .. vim.fn.shellescape(module) },
             {
               stdout_buffered = true,
               on_stdout = function(_, data, _)
@@ -59,7 +73,7 @@ return {
             }
           )
           vim.keymap.set("n", "q", "<cmd>close<CR>", { buffer = buf, nowait = true })
-        end, { desc = 'ansible-doc', buffer = args.buf })
+        end, { desc = 'ansible-doc', buffer = ev.buf })
       end,
     })
   end,
