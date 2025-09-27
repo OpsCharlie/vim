28f1c13912b8e2f31fda579baa52359f7f3ccabb
OpsCharlie <verstraete.carl@gmail.com>
2025-09-27 18:17:17 +0200

    enable statuscolumn

---
+++ lua/config/snacks/statuscolumn.lua
@@ -0,0 +1,8 @@
+return {
+	left = { "mark", "sign" }, -- priority of signs on the left (high to low)
+	right = { "fold", "git" }, -- priority of signs on the right (high to low)
+	folds = {
+		open = true, -- show open fold icons
+		git_hl = true, -- use Git Signs hl for fold icons
+	},
+}
