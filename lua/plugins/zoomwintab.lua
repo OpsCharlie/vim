b5267843d54eabf2de33c71feb6bd141d72e6f58
OpsCharlie <verstraete.carl@gmail.com>
2025-09-27 21:57:21 +0200

    Remove files

--- lua/plugins/zoomwintab.lua
+++
@@ -1,9 +0,0 @@
-return {}
--- return {
---   "troydm/zoomwintab.vim",
---   -- A simple zoom window
---   lazy = true,
---   keys = {
---     { "<C-w>z", ":ZoomWinTabToggle<CR>", desc = "Toggle zoom" },
---   },
--- }
