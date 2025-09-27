9e9a20b12d7057368a203c5bbab95bc28614cd64
OpsCharlie <verstraete.carl@gmail.com>
2025-09-27 18:17:39 +0200

    Update ufo

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -8,20 +8,20 @@ return {
   "kevinhwang91/nvim-ufo",
   dependencies = {
     "kevinhwang91/promise-async",
-    {
-      "luukvbaal/statuscol.nvim",
-      config = function()
-        local builtin = require("statuscol.builtin")
-        require("statuscol").setup({
-          relculright = true,
-          segments = {
-            { text = { builtin.foldfunc },      click = "v:lua.ScFa" },
-            { text = { "%s" },                  click = "v:lua.ScSa" },
-            { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
-          },
-        })
-      end,
-    },
+    -- {
+    --   "luukvbaal/statuscol.nvim",
+    --   config = function()
+    --     local builtin = require("statuscol.builtin")
+    --     require("statuscol").setup({
+    --       relculright = true,
+    --       segments = {
+    --         { text = { builtin.foldfunc },      click = "v:lua.ScFa" },
+    --         { text = { "%s" },                  click = "v:lua.ScSa" },
+    --         { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+    --       },
+    --     })
+    --   end,
+    -- },
   },
   event = "BufReadPost",
   keys = {
@@ -37,7 +37,7 @@ return {
 
   init = function()
     vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-    vim.o.foldcolumn = "1"     -- '0' is not bad
+    -- vim.o.foldcolumn = "1"     -- '0' is not bad
     vim.o.foldlevel = 99       -- Using ufo provider need a large value, feel free to decrease the value
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
