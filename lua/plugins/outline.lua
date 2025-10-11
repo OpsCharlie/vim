7797708fb100eebc0903d5856daf9aee529b72e6
OpsCharlie <verstraete.carl@gmail.com>
2025-10-11 17:53:57 +0200

    Configure outline with treesitter provider

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -1,9 +1,21 @@
 return {
   "hedyhli/outline.nvim",
+  dependencies = {
+    'epheien/outline-treesitter-provider.nvim'
+  },
   lazy = true,
   cmd = { "Outline", "OutlineOpen" },
   keys = {
     { "<leader>tO", "<cmd>Outline<CR>", desc = "Outline" },
   },
-  opts = {},
+  opts = {
+    providers = {
+      priority = { 'lsp', 'markdown', 'norg', 'treesitter' },
+    },
+    symbols = {
+      filter = {
+        sh = { 'String', 'Variable', exclude = true },  -- Exclude String and Variable from sh files
+      },
+    },
+  },
 }
