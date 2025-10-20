5761698a693901ca6622c9518b4e06b771ab71c4
OpsCharlie <verstraete.carl@gmail.com>
2025-10-20 12:40:37 +0200

    Update lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -15,15 +15,12 @@ return {
     opts = {},
   },
   {
-    "folke/lazydev.nvim",
-    -- Useful to load neovim lua environment.
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-      },
-    },
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    -- Automatically install and manage LSP servers, DAP servers, linters, and formatters.
+    lazy = true,
+    -- event = { "BufReadPost", "BufNewFile", "FileType" },
+    cmd = { "MasonToolsInstall", "MasonToolsUpdate" },
+    dependencies = { "mason-org/mason.nvim" },
+    opts = require("config.mason"),
   },
 }
