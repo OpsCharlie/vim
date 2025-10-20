b3efd7dd6460f5690ac8b2be8c262e293a0b2074
OpsCharlie <verstraete.carl@gmail.com>
2025-10-20 12:42:37 +0200

    update trouble

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,13 +1,40 @@
 return {
   "folke/trouble.nvim",
-  -- A pretty diagnostics, references, telescope results, quickfix and location list to help you solve all the trouble your code is causing.
+  lazy = true,
+  opts = {
+    auto_close = true,
+    modes = {
+      symbols = {
+        format = "{kind_icon}{symbol.name}",
+      },
+    },
+  },
   cmd = "Trouble",
-  opts = {},
   keys = {
     {
-      "<leader>dD",
-      "<cmd>Trouble diagnostics toggle<cr>",
+      "<leader>td",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
       desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>ts",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols LSP (Trouble)",
+    },
+    {
+      "<leader>tl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>tL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>tQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
     },
   },
 }
