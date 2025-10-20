e0818c9887bfd5feb0d999da29dbabf2c358d361
OpsCharlie <verstraete.carl@gmail.com>
2025-10-20 12:42:47 +0200

    Update which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,7 +3,13 @@ return {
   -- WhichKey helps you remember your Neovim keymaps
   lazy = false,
   keys = {
-    { "<leader>?", function() require("which-key").show({ global = false }) end, desc = "Buffer Local Keymaps (which-key)", },
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = true })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
   },
   config = function()
     require("which-key").setup({
@@ -14,6 +20,14 @@ return {
       --   margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
       --   padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
       -- },
+    })
+
+    local wk = require("which-key")
+    wk.add({
+      { "<leader>t", group = "Toggle" },
+      { "<leader>c", group = "Code" },
+      { "<leader>d", group = "Dap" },
+      { "<leader>o", group = "Opencode" },
     })
   end,
 }
