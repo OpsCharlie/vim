ec50f1f31149d0bccf50258816cd073cc55b6e47
OpsCharlie <verstraete.carl@gmail.com>
2026-03-01 13:38:22 +0100

    Update treesitter with context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,38 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  branch = "main",
-  version = false,
-  build = ":TSUpdate",
-  config = function()
-    local file_size = vim.fn.getfsize(vim.fn.expand('%'))
-    local max_size = vim.g.bigfile_size_limit or (1024 * 1024 * 50)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    branch = "main",
+    version = false,
+    build = ":TSUpdate",
+    config = function()
+      local file_size = vim.fn.getfsize(vim.fn.expand('%'))
+      local max_size = vim.g.bigfile_size_limit or (1024 * 1024 * 50)
 
-    if file_size < max_size then
-      local config = require("config.treesitter")
-      require("nvim-treesitter.install").compilers = { "gcc", "clang" }
+      if file_size < max_size then
+        local config = require("config.treesitter")
+        require("nvim-treesitter.install").compilers = { "gcc", "clang" }
 
-      require("nvim-treesitter").setup({
-        highlight = config.highlight,
-        indent = config.indent,
-      })
+        require("nvim-treesitter").setup({
+          highlight = config.highlight,
+          indent = config.indent,
+        })
 
-      vim.defer_fn(function()
-        require("nvim-treesitter").install(config.parsers)
-      end, 0)
-    end
-  end,
+        vim.defer_fn(function()
+          require("nvim-treesitter").install(config.parsers)
+        end, 0)
+      end
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    lazy = true,
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = {
+      enable = true,
+      max_lines = 3,
+      trim_scope = "outer",
+    },
+  },
 }
