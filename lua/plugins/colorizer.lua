1ab1237021c0e9e33e76efb8047dbd4bb7eb6767
OpsCharlie <verstraete.carl@gmail.com>
2026-03-01 13:43:39 +0100

    Use new format

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -5,20 +5,21 @@ return {
   config = function()
     require("colorizer").setup({
       filetypes = { "*", "!prompt", "!popup" },
-      user_default_options = {
-        RGB = true,
-        RRGGBB = true,
-        names = false,
-        RRGGBBAA = true,
-        AARRGGBB = false,
-        rgb_fn = true,
-        hsl_fn = true,
-        css = false,
-        css_fn = false,
-        mode = "background",
-        tailwind = true,
+      options = {
+        parsers = {
+          css = true,
+          css_fn = true,
+          names = { enable = false },
+          hex = { enable = true, rgb = true, rrggbb = true, rrggbbaa = true, aarrggbb = false },
+          rgb = { enable = true },
+          hsl = { enable = true },
+          oklch = { enable = true },
+          tailwind = { enable = true },
+        },
+        display = {
+          mode = "background",
+        },
       },
-      buftypes = {},
     })
   end,
 }
