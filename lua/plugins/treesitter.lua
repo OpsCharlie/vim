fc70bc5904af7886d573a1a9ab33344ebea8aff0
OpsCharlie <verstraete.carl@gmail.com>
2026-03-01 09:33:11 +0100

    Update treesitter to main

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,58 +1,25 @@
- return {
-   "nvim-treesitter/nvim-treesitter",
-   -- Nvim Treesitter configurations and abstraction layer
-   -- dependencies = {
-   --   { "nvim-treesitter/nvim-treesitter-context", },
-   -- },
+return {
+  "nvim-treesitter/nvim-treesitter",
   lazy = false,
+  branch = "main",
+  version = false,
   build = ":TSUpdate",
   config = function()
     local file_size = vim.fn.getfsize(vim.fn.expand('%'))
     local max_size = vim.g.bigfile_size_limit or (1024 * 1024 * 50)
 
     if file_size < max_size then
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-         ensure_installed = {
-           "bash",
-           "desktop",
-           "go",
-           "json",
-           "lua",
-           "markdown",
-           "markdown_inline",
-           "php",
-           "powershell",
-           "python",
-           "regex",
-           "yaml",
-           "diff",
-           "dockerfile",
-           "git_config",
-           "gitcommit",
-           "gitignore",
-           "jinja",
-           "jinja_inline",
-           "terraform",
-           "vim",
-           "vimdoc",
-         },
-        sync_install = false,
-        ignore_install = {},
-        modules = {},
-        auto_install = true,
-        highlight = {
-          enable = true,
-          disable = { "yaml" },
-        },
-        indent = {
-          enable = true,
-          disable = { "yaml" },
-        },
-        matchup = {
-          enable = true,         -- mandatory, false will disable the whole extension
-        },
+      local config = require("config.treesitter")
+      require("nvim-treesitter.install").compilers = { "gcc", "clang" }
+
+      require("nvim-treesitter").setup({
+        highlight = config.highlight,
+        indent = config.indent,
       })
+
+      vim.defer_fn(function()
+        require("nvim-treesitter").install(config.parsers)
+      end, 0)
     end
   end,
 }
